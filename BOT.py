9af6f6cdc8fb91ab2ddaf6a24d7ba48bc201c16a
leosabbadim <71013611+leosabbadim@users.noreply.github.com>
2020-12-12 15:35:48 -0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -3,8 +3,6 @@
 import math
 import networkx as nx
 import nltk
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 import time
 import io
 from pdfminer.converter import TextConverter
@@ -165,46 +163,6 @@ class Texto:
 
         return ' '.join([s.raw_text for s in ordenadas])
 
-bot = ChatBot("Bot Acadêmico")  # inicia o bot
-conversa = ['olá',
-'oi',
-'como vai você?',
-'eu estou bem',
-'que bom',
-'sim',
-'posso ajudá-lo com alguma coisa?',
-'sim, eu tenho uma pergunta',
-'qual é a sua pergunta?',
-'eu poderia pedir uma xícara de açúcar?',
-'me desculpe, mas eu não tenho nenhum',
-'obrigado de qualquer maneira',
-'sem problemas',
-'qual é o seu livro favorito?',
-'sou um computador, não tenho preferências',
-'então, qual é a sua cor favorita?',
-'roxo',
-'quem é você?',
-'quem? Quem é senão uma forma seguindo a função de quê',
-'então o que você é?',
-'um homem em uma máscara',
-'eu posso ver isso',
-'não é seu poder de observação eu duvido, mas apenas a natureza paradoxal de pedir um homem mascarado que é. Mas diga-me, você gosta de música?',
-'eu gosto de ver filmes',
-'que tipo de filme você gosta?',
-'eu gosto do Exterminador do Futuro',
-'eu estou trabalhando em um projeto',
-'qual projeto?',
-'eu estou tentando fazer um bolo',
-'fale-me sobre você',
-'o que você quer saber?',
-'você é um robô?',
-'sim eu sou',
-'como você trabalha?',
-'complexo demais para você entender']
-
-trainer = ListTrainer(bot)  # define o método de treinamento
-trainer.train(conversa)  # define a lista de conversa
-
 saudacao()
 print('\n\tEu sou o Bot Acadêmico, um bot em treinamento \n'
       '\tSou programado para resumir artigo e textos \n'
@@ -249,291 +207,268 @@ def enviar_email(text_entrada):
     server.quit()
     print("\nE-mail enviado!")
 
+print('\nMe diz o nome do arquivo que você quer que eu busque: '
+          '\nLembrando: Sempre no formato PDF')
+PDFfile = input('Artigo: ')
 
-while True:
-    quest = input('Humano: ').lower()
-    if (quest == 'tchau'):
-        resposta = ('Adorei conversar com você, volte sempre!')
-        print('Bot Acadêmico: ', resposta)
-        exit()
+inPDFfile = (PDFfile + '.pdf')
 
-    if (quest == 'leitura'):
+num_autor = int(input('Quantos autores esse artigo tem: '))
+autores = []
+count = 0
+print('Ok, agora me informa o nome desses autores dando ENTER entre cada um deles e no final\n')
+while count < num_autor:
+    autor = input('Autor(a): ')
+    autores.append(autor)
+    count = count + 1
 
-        print('\nMe diz o nome do arquivo que você quer que eu busque: '
-              '\nLembrando: Sempre no formato PDF')
-        PDFfile = input('Artigo: ')
+lerPDF = ler_pdf(inPDFfile)
 
-        inPDFfile = (PDFfile+'.pdf')
+t = Texto(lerPDF)
+resumo = (t.resumir())
 
-        num_autor = int(input('Quantos autores esse artigo tem: '))
-        autores = []
-        count = 0
-        print('Ok, agora me informa o nome desses autores dando ENTER entre cada um deles e no final\n')
-        while count < num_autor:
-            autor = input('Autor(a): ')
-            autores.append(autor)
-            count = count + 1
+print("\n\nCerto, agora vou pesquisar o ineditismo desse artigo...")
 
-        lerPDF = ler_pdf(inPDFfile)
+####################
 
-        t = Texto(lerPDF)
-        resumo = (t.resumir())
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+# Chamando a função de pesquisa por texto
+PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
+driver = webdriver.Chrome(PATH)
 
-        print("\n\nCerto, agora vou pesquisar o ineditismo desse artigo...")
+driver.get('https://scholar.google.com.br/')
+search = driver.find_element_by_id("gs_hdr_tsi")
+search.send_keys(resumo)
+# search.send_keys(Keys.RETURN)
+result = driver.find_element_by_id("gs_ab_md")
 
-                                ####################
+x = (result.text).split()
+driver.quit()
 
-        from selenium import webdriver
-        from selenium.webdriver.common.keys import Keys
+# Caso tenha poucos resultados de texto
+if x == []:
+    print('Pesquisei pelo resumo e não encontrei nada, vou dar uma certificada no resultado'
+          'Vou pesquisar pelo texto em sí, me da 1 min')
+    PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
-        # Chamando a função de pesquisa por texto
-        PATH = 'C:\Program Files (x86)\chromedriver.exe'
+    driver = webdriver.Chrome(PATH)
 
-        driver = webdriver.Chrome(PATH)
+    driver.get('https://scholar.google.com.br/')
+    search = driver.find_element_by_id("gs_hdr_tsi")
+    search.send_keys(lerPDF)
+    # search.send_keys(Keys.RETURN)
+    result = driver.find_element_by_id("gs_ab_md")
 
-        driver.get('https://scholar.google.com.br/')
-        search = driver.find_element_by_id("gs_hdr_tsi")
-        search.send_keys(resumo)
-        #search.send_keys(Keys.RETURN)
-        result = driver.find_element_by_id("gs_ab_md")
+    x = (result.text).split()
+    driver.quit()
 
-        x = (result.text).split()
-        driver.quit()
+    if x == []:
+        pontos1 = 5
+        x_result = '0 resultados'
+        est1 = (u'\u2606' * pontos1)
+    elif x[1] == 'resultado':
+        x_result = (x[0] + ' ' + x[1])
+        x_f = (float(x[0].replace('.', '')))
+        y = (int(x_f))
 
-
-        # Caso tenha poucos resultados de texto
-        if x == []:
-            print('Pesquisei pelo resumo e não encontrei nada, vou dar uma certificada no resultado'
-                  'Vou pesquisar pelo texto em sí, me da 1 min')
-            PATH = 'C:\Program Files (x86)\chromedriver.exe'
-
-            driver = webdriver.Chrome(PATH)
-
-            driver.get('https://scholar.google.com.br/')
-            search = driver.find_element_by_id("gs_hdr_tsi")
-            search.send_keys(lerPDF)
-            #search.send_keys(Keys.RETURN)
-            result = driver.find_element_by_id("gs_ab_md")
-
-            x = (result.text).split()
-            driver.quit()
-
-            if x == []:
-                pontos1 = 5
-                x_result = '0 resultados'
-                est1 = (u'\u2606' * pontos1)
-            elif x[1] == 'resultado':
-                x_result = (x[0] + ' ' + x[1])
-                x_f = (float(x[0].replace('.', '')))
-                y = (int(x_f))
-
-                if y >= 500000:
-                    pontos1 = +1
-                elif y >= 100000 and y < 500000:
-                    pontos1 = +2
-                elif y >= 50000 and y < 100000:
-                    pontos1 = +3
-                elif y >= 10000 and y < 30000:
-                    pontos1 = +4
-                else:
-                    pontos1 = +5
-
-                est1 = (u'\u2606' * pontos1)
-
-
-            # Busca de texto normal
-            else:
-                x_result = (x[1] + ' ' + x[2])
-                x_f = (float(x[1].replace('.', '')))
-                y = (int(x_f))
-
-                pontos1 = None
-
-                if y >= 500000:
-                    pontos1 = +1
-                elif y >= 100000 and y < 500000:
-                    pontos1 = +2
-                elif y >= 50000 and y < 100000:
-                    pontos1 = +3
-                elif y >= 10000 and y < 30000:
-                    pontos1 = +4
-                else:
-                    pontos1 = +5
-
-                est1 = (u'\u2606' * pontos1)
-
-
-        if x[1] == 'resultado':
-            x_result = (x[0] + ' ' + x[1])
-            x_f = (float(x[0].replace('.', '')))
-            y = (int(x_f))
-
-            if y >= 500000:
-                pontos1 = +1
-            elif y >= 100000 and y < 500000:
-                pontos1 = +2
-            elif y >= 50000 and y < 100000:
-                pontos1 = +3
-            elif y >= 10000 and y < 30000:
-                pontos1 = +4
-            else:
-                pontos1 = +5
-
-            est1 = (u'\u2606' * pontos1)
-
-        elif x[0] == 'Aproximadamente':
-            x_result = (x[1] + ' ' + x[2])
-            x_f = (float(x[1].replace('.', '')))
-            y = (int(x_f))
-
-            if y >= 500000:
-                pontos1 = +1
-            elif y >= 100000 and y < 500000:
-                pontos1 = +2
-            elif y >= 50000 and y < 100000:
-                pontos1 = +3
-            elif y >= 10000 and y < 30000:
-                pontos1 = +4
-            else:
-                pontos1 = +5
-
-            est1 = (u'\u2606' * pontos1)
-
-
-        # Busca de texto normal
+        if y >= 500000:
+            pontos1 = +1
+        elif y >= 100000 and y < 500000:
+            pontos1 = +2
+        elif y >= 50000 and y < 100000:
+            pontos1 = +3
+        elif y >= 10000 and y < 30000:
+            pontos1 = +4
         else:
-            x_result = (x[1] + ' ' + x[2])
-            x_f = (float(x[1].replace('.', '')))
+            pontos1 = +5
+
+        est1 = (u'\u2606' * pontos1)
+
+
+    # Busca de texto normal
+    else:
+        x_result = (x[1] + ' ' + x[2])
+        x_f = (float(x[1].replace('.', '')))
+        y = (int(x_f))
+
+        pontos1 = None
+
+        if y >= 500000:
+            pontos1 = +1
+        elif y >= 100000 and y < 500000:
+            pontos1 = +2
+        elif y >= 50000 and y < 100000:
+            pontos1 = +3
+        elif y >= 10000 and y < 30000:
+            pontos1 = +4
+        else:
+            pontos1 = +5
+
+        est1 = (u'\u2606' * pontos1)
+
+if x[1] == 'resultado':
+    x_result = (x[0] + ' ' + x[1])
+    x_f = (float(x[0].replace('.', '')))
+    y = (int(x_f))
+
+    if y >= 500000:
+        pontos1 = +1
+    elif y >= 100000 and y < 500000:
+        pontos1 = +2
+    elif y >= 50000 and y < 100000:
+        pontos1 = +3
+    elif y >= 10000 and y < 30000:
+        pontos1 = +4
+    else:
+        pontos1 = +5
+
+    est1 = (u'\u2606' * pontos1)
+
+elif x[0] == 'Aproximadamente':
+    x_result = (x[1] + ' ' + x[2])
+    x_f = (float(x[1].replace('.', '')))
+    y = (int(x_f))
+
+    if y >= 500000:
+        pontos1 = +1
+    elif y >= 100000 and y < 500000:
+        pontos1 = +2
+    elif y >= 50000 and y < 100000:
+        pontos1 = +3
+    elif y >= 10000 and y < 30000:
+        pontos1 = +4
+    else:
+        pontos1 = +5
+
+    est1 = (u'\u2606' * pontos1)
+
+    # Busca de texto normal
+else:
+    x_result = (x[1] + ' ' + x[2])
+    x_f = (float(x[1].replace('.', '')))
+    y = (int(x_f))
+
+    pontos1 = None
+
+    if y >= 500000:
+        pontos1 = +1
+    elif y >= 100000 and y < 500000:
+        pontos1 = +2
+    elif y >= 50000 and y < 100000:
+        pontos1 = +3
+    elif y >= 10000 and y < 30000:
+        pontos1 = +4
+    else:
+        pontos1 = +5
+
+    est1 = (u'\u2606' * pontos1)
+
+    ###
+    ### Chamando a função de pesquisa por nome
+print("\nEspera só mais um pouco, vou pesquisar sobre os autores desse artigo...")
+time.sleep(2)
+var_autor = []
+
+for autor in autores:
+
+    PATH = 'C:\Program Files (x86)\chromedriver.exe'
+    driver = webdriver.Chrome(PATH)
+    driver.get('https://scholar.google.com.br/')
+
+    search = driver.find_element_by_id("gs_hdr_tsi")
+    search.send_keys('"a"'.replace("a", autor))
+    search.send_keys(Keys.RETURN)
+
+    name = driver.find_element_by_id("gs_ab_md")
+
+    if name.text == '':
+        pontos2 = 1
+        z_result = '0 resultados'
+        est2 = (u'\u2606' * pontos2)
+
+    else:
+        z = (name.text).split()
+
+        # Pesquisa para poucos resultados de autor
+        if z[1] == 'resultado':
+            z_result = (z[0] + ' ' + z[1])
+            x_f = (float((z[0].replace('.', ''))))
+            y = (int(z[0]))
+
+            if y >= 20:
+                pontos2 = 5
+            elif y >= 15 and y < 20:
+                pontos2 = 4
+            elif y >= 10 and y < 15:
+                pontos2 = 3
+            elif y >= 5 and y < 10:
+                pontos2 = 2
+            else:
+                pontos2 = 1
+
+            est2 = (u'\u2606' * pontos2)
+
+        # Pesquisa normal de autor
+        elif z[0] == 'Aproximadamente':
+            z_result = (z[1] + ' ' + z[2])
+            x_f = (float((z[1].replace('.', ''))))
             y = (int(x_f))
 
-            pontos1 = None
-
-            if y >= 500000:
-                pontos1 = +1
-            elif y >= 100000 and y < 500000:
-                pontos1 = +2
-            elif y >= 50000 and y < 100000:
-                pontos1 = +3
-            elif y >= 10000 and y < 30000:
-                pontos1 = +4
+            if y >= 20:
+                pontos2 = 5
+            elif y >= 15 and y < 20:
+                pontos2 = 4
+            elif y >= 10 and y < 15:
+                pontos2 = 3
+            elif y >= 5 and y < 10:
+                pontos2 = 2
             else:
-                pontos1 = +5
-
-            est1 = (u'\u2606' * pontos1)
-
-
-        ###
-        ### Chamando a função de pesquisa por nome
-        print("\nEspera só mais um pouco, vou pesquisar sobre os autores desse artigo...")
-        time.sleep(2)
-        var_autor = []
-
-        for autor in autores:
-
-            PATH = 'C:\Program Files (x86)\chromedriver.exe'
-            driver = webdriver.Chrome(PATH)
-            driver.get('https://scholar.google.com.br/')
-
-            search = driver.find_element_by_id("gs_hdr_tsi")
-            search.send_keys('"a"'.replace("a", autor))
-            search.send_keys(Keys.RETURN)
-
-            name = driver.find_element_by_id("gs_ab_md")
-
-
-            if name.text == '':
                 pontos2 = 1
-                z_result = '0 resultados'
-                est2 = (u'\u2606' * pontos2)
 
+            est2 = (u'\u2606' * pontos2)
+
+        else:
+            z_result = (z[0] + ' ' + z[1])
+            y = (int(z[0]))
+
+            if y >= 20:
+                pontos2 = 5
+            elif y >= 15 and y < 20:
+                pontos2 = 4
+            elif y >= 10 and y < 15:
+                pontos2 = 3
+            elif y >= 5 and y < 10:
+                pontos2 = 2
             else:
-                z = (name.text).split()
+                pontos2 = 1
 
-                # Pesquisa para poucos resultados de autor
-                if z[1] == 'resultado':
-                    z_result = (z[0] + ' ' + z[1])
-                    x_f = (float((z[0].replace('.', ''))))
-                    y = (int(z[0]))
+            est2 = (u'\u2606' * pontos2)
 
-                    if y >= 20:
-                        pontos2 = 5
-                    elif y >= 15 and y < 20:
-                        pontos2 = 4
-                    elif y >= 10 and y < 15:
-                        pontos2 = 3
-                    elif y >= 5 and y < 10:
-                        pontos2 = 2
-                    else:
-                        pontos2 = 1
+driver.quit()
+resultado = (
+        'O(a) Autor(a) ' + autor + ' obteve ' + z_result + ', alcançado a seguinte pontuação: ' +
+        est2)
+var_autor.append(resultado)
 
-                    est2 = (u'\u2606' * pontos2)
+texto_expor = [('\n\n\n'),
+                ('A pontuação foi feita de 1 a 5, sendo 1 a menor pontuação e 5 a maior'),
+                ('Para o texto, quanto menor o resultado encontrado, melhor a pontuação'),
+                ('Para o autor, quanto maior o resultado encontrado, melhor a pontuação')]
 
-                # Pesquisa normal de autor
-                elif z[0] == 'Aproximadamente':
-                    z_result = (z[1] + ' ' + z[2])
-                    x_f = (float((z[1].replace('.', ''))))
-                    y = (int(x_f))
+resultado_texto = ('O texto pesquisado obteve ' + x_result + ', e alcançou a seguinte pontuação: ' + est1)
 
-                    if y >= 20:
-                        pontos2 = 5
-                    elif y >= 15 and y < 20:
-                        pontos2 = 4
-                    elif y >= 10 and y < 15:
-                        pontos2 = 3
-                    elif y >= 5 and y < 10:
-                        pontos2 = 2
-                    else:
-                        pontos2 = 1
+texto_expor_novo = "\n".join(map(str, texto_expor))
+resultado_texto_novo = "\n".join(map(str, resultado_texto))
+var_autor_novo = "\n".join(map(str, var_autor))
 
-                    est2 = (u'\u2606' * pontos2)
+entrada = ['O artigo pesquisado foi:', PDFfile, 'Resumo: ', resumo, texto_expor_novo, resultado_texto,
+            var_autor_novo]
+# text_entrada = ('O resumo feito foi: \r\n' + resumo + str(texto_expor) + '\r\n'+ str(resultado_texto)+ '\r\n'+ str(var_autor))
 
-                else:
-                    z_result = (z[0] + ' ' + z[1])
-                    y = (int(z[0]))
+text_entrada = "\n\n".join(map(str, entrada))
 
-                    if y >= 20:
-                        pontos2 = 5
-                    elif y >= 15 and y < 20:
-                        pontos2 = 4
-                    elif y >= 10 and y < 15:
-                        pontos2 = 3
-                    elif y >= 5 and y < 10:
-                        pontos2 = 2
-                    else:
-                        pontos2 = 1
-
-                    est2 = (u'\u2606' * pontos2)
-
-            driver.quit()
-            resultado = (
-                        'O(a) Autor(a) ' + autor + ' obteve ' +  z_result + ', alcançado a seguinte pontuação: '+
-                        est2)
-            var_autor.append(resultado)
-
-
-        texto_expor= [('\n\n\n'),
-                      ('A pontuação foi feita de 1 a 5, sendo 1 a menor pontuação e 5 a maior'),
-                      ('Para o texto, quanto menor o resultado encontrado, melhor a pontuação'),
-                      ('Para o autor, quanto maior o resultado encontrado, melhor a pontuação')]
-
-        resultado_texto = ('O texto pesquisado obteve ' + x_result + ', e alcançou a seguinte pontuação: ' + est1)
-
-        texto_expor_novo = "\n".join(map(str, texto_expor))
-        resultado_texto_novo = "\n".join(map(str, resultado_texto))
-        var_autor_novo = "\n".join(map(str, var_autor))
-
-        entrada = ['O artigo pesquisado foi:', PDFfile, 'Resumo: ', resumo, texto_expor_novo, resultado_texto, var_autor_novo]
-        #text_entrada = ('O resumo feito foi: \r\n' + resumo + str(texto_expor) + '\r\n'+ str(resultado_texto)+ '\r\n'+ str(var_autor))
-
-        text_entrada = "\n\n".join(map(str, entrada))
-
-        email_enviar = input('\nInforme um e-mail para enviar os resultados: ')
-
-        enviar_email(text_entrada)
-
-    resposta = bot.get_response(quest)
-    print('Bot Acadêmico: ', resposta)
-
-
+email_enviar = input('\nInforme um e-mail para enviar os resultados: ')
+enviar_email(text_entrada)
